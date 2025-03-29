7742027ac75c21a928c75110ec6d649efc6303af
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-03-29 14:18:15 +0200

    Adding comments to functions

--- MobileApp/functions/validateEmail.js
+++ MobileApp/functions/validateEmail.js
@@ -1,12 +1,15 @@
 export function validateEmail(emailInput) {
+    //Checks if email field is empty
     if (emailInput === "") {
         return "You have to insert password in order to continue!";
     }
 
+    //Checks if email is at least 7 symbols long
     if (emailInput.length < 7) {
         return "Too short email!";
     }
 
+    //Regex for checking whether the email is valid
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(emailInput)) {
         return "You have to provide a valid email!";
--- MobileApp/functions/validatePassword.js
+++ MobileApp/functions/validatePassword.js
@@ -1,16 +1,20 @@
 export function validatePassword(passwordInput) {
+    //Checks if password field is empty
     if (passwordInput === "") {
         return "You have to insert password in order to continue!";
     }
 
+    //Checks if password is at least 7 symbols
     if (passwordInput.length < 7) {
         return "Password should be at least 7 symbols!";
     }
 
+    //Checks if password is not just numbers
     if (/^[0-9]+$/.test(passwordInput)) {
         return "The password should not contains just numbers!";
     }
 
+    //Checks if password contains at least 1 special symbol
     const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
     if (!specialCharRegex.test(passwordInput)) {
         return "The password should contain at least 1 special symbol!";
