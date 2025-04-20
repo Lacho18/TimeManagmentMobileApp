e195ae5a5713085debf09c3ab204c8c062ebb160
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-04-20 19:12:52 +0300

    Adding profile header

--- MobileApp/app/home/profile.jsx
+++ MobileApp/app/home/profile.jsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { GLOBAL_STYLES } from "@/constants/PageStyle";
 import { useUser } from "../../context/UserContext";
@@ -8,15 +8,41 @@ export default function Profile() {
   const { user } = useUser();
 
   console.log("Q puk stane");
-  console.log(user);
+  console.log(user.image);
 
   return (
-    <View style={{ ...GLOBAL_STYLES.page, backgroundColor: theme.background }}>
-      <View>
-        <Image />
-        <Text></Text>
+    <View style={{ ...styles.page, backgroundColor: theme.background }}>
+      <View style={styles.headerDiv}>
+        <Image style={styles.profileImage} source={{ uri: user.image }} />
+        <Text>{user.name}</Text>
       </View>
       <Text>Profile</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  headerDiv: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 18,
+    alignItems: "center",
+    width: "100%",
+    height: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+    boxSizing: "border-box",
+    backgroundColor: "green",
+  },
+
+  profileImage: {
+    width: 30,
+    height: 30,
+    borderRadius: "50%",
+  },
+});
