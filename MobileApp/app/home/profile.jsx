f8271392aa29b55086159abeeba9a536950f0ea7
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-04-20 19:17:09 +0300

    Adding styling to profile header

--- MobileApp/app/home/profile.jsx
+++ MobileApp/app/home/profile.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { GLOBAL_STYLES } from "@/constants/PageStyle";
 import { useUser } from "../../context/UserContext";
@@ -10,39 +10,46 @@ export default function Profile() {
   console.log("Q puk stane");
   console.log(user.image);
 
+  const styles = StyleSheet.create({
+    page: {
+      flex: 1,
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+    },
+    headerDiv: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 18,
+      alignItems: "center",
+      width: "100%",
+      height: 40,
+      paddingLeft: 20,
+      paddingRight: 20,
+      boxSizing: "border-box",
+      backgroundColor: theme.primary,
+    },
+    profileImage: {
+      width: 30,
+      height: 30,
+      borderRadius: "50%",
+    },
+    titleText: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: theme.text,
+    },
+  });
+
   return (
     <View style={{ ...styles.page, backgroundColor: theme.background }}>
       <View style={styles.headerDiv}>
         <Image style={styles.profileImage} source={{ uri: user.image }} />
-        <Text>{user.name}</Text>
+        <Text style={styles.titleText}>{user.name}</Text>
+      </View>
+      <View>
+        <TouchableOpacity></TouchableOpacity>
       </View>
       <Text>Profile</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  },
-  headerDiv: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 18,
-    alignItems: "center",
-    width: "100%",
-    height: 40,
-    paddingLeft: 20,
-    paddingRight: 20,
-    boxSizing: "border-box",
-    backgroundColor: "green",
-  },
-
-  profileImage: {
-    width: 30,
-    height: 30,
-    borderRadius: "50%",
-  },
-});
