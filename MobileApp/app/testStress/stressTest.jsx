00c10ff7cd13aff5a95559812ba4ef7ec7e81b9e
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-05-08 22:01:21 +0300

    Creating context for thew stress test

--- MobileApp/app/testStress/stressTest.jsx
+++ MobileApp/app/testStress/stressTest.jsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { useStressTest } from "../../context/StressTestContext";
+import SingleAnswerQuestion from "../../components/StressTest/SingleAnswerQuestion";
+import { useState } from "react";
 
 export default function StressTest() {
   const { theme } = useTheme();
   const { stressTestQuestions } = useStressTest();
+  const [index, setIndex] = useState(0);
 
   console.log(stressTestQuestions);
 
@@ -15,12 +18,24 @@ export default function StressTest() {
       backgroundColor: theme.background,
       position: "absolute",
       zIndex: 100,
+      display: "flex",
+      alignItems: "center",
+      paddingVertical: 130,
+      paddingHorizontal: 30,
+      justifyContent: "space-around",
+    },
+    title: {
+      fontSize: 55,
+      fontWeight: "bold",
+      textTransform: "uppercase",
+      color: theme.text,
     },
   });
 
   return (
     <View style={styles.mainDiv}>
-      <Text>123</Text>
+      <Text style={styles.title}>Stress test</Text>
+      <SingleAnswerQuestion question={stressTestQuestions[0]} theme={theme} />
     </View>
   );
 }
--- MobileApp/components/StressTest/SingleAnswerQuestion.jsx
+++ MobileApp/components/StressTest/SingleAnswerQuestion.jsx
@@ -1,14 +1,51 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function SingleAnswerQuestion({ question, theme }) {
+  const styles = StyleSheet.create({
+    mainDiv: {
+      alignSelf: "flex-start",
+      display: "flex",
+      gap: 50,
+      width: "100%",
+    },
+    questionText: {
+      fontSize: 30,
+      color: theme.text,
+      textAlign: "center",
+    },
+    allAnswersDiv: {
+      display: "flex",
+      gap: 20,
+    },
+    answerDiv: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 20,
+      alignItems: "center",
+      width: "100%",
+    },
+    answerBox: {
+      width: 20,
+      height: 20,
+      borderRadius: "50%",
+      borderWidth: 1,
+      backgroundColor: "white",
+    },
+    answerText: {
+      paddingBottom: 5,
+      fontSize: 25,
+      color: theme.text,
+    },
+  });
+
   return (
-    <View>
-      <Text>{question.question}</Text>
-      <View>
+    <View style={styles.mainDiv}>
+      <Text style={styles.questionText}>{question.question}</Text>
+      <View style={styles.allAnswersDiv}>
         {question.answers.map((answer) => (
-          <View>
-            <Pressable></Pressable>
-            <Text>{answer}</Text>
+          <View style={styles.answerDiv}>
+            <Pressable style={styles.answerBox}></Pressable>
+            <Text style={styles.answerText}>{answer}</Text>
           </View>
         ))}
       </View>
