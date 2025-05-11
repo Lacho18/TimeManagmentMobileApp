2fe6a56c9448a7a631489b1efdf0a5392c70fb78
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-05-11 19:29:17 +0300

    Making the stress test submit button to go back to the previous page

--- MobileApp/app/home/events.jsx
+++ MobileApp/app/home/events.jsx
@@ -1,12 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { GLOBAL_STYLES } from "@/constants/PageStyle";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Events() {
   const { theme } = useTheme();
+  const [googleToken, setGoogleToken] = useState(null);
+
+  useEffect(() => {
+    async function getGoogleToken() {
+      const token = await AsyncStorage.getItem("@token");
+
+      if (token) {
+        setGoogleToken(token);
+      }
+    }
+
+    getGoogleToken();
+  }, []);
+
+  console.log(googleToken);
+
+  const styles = StyleSheet.create({
+    mainDiv: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    tasksContainerStyle: {
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      gap: 20,
+    },
+  });
+
   return (
-    <View style={{ ...GLOBAL_STYLES.page, backgroundColor: theme.background }}>
+    <ScrollView
+      style={styles.mainDiv}
+      contentContainerStyle={styles.tasksContainerStyle}
+    >
       <Text>Events</Text>
-    </View>
+    </ScrollView>
   );
 }
