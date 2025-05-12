0a06b637b9a4ebc699e1dd4109684f290e031d07
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-05-12 12:01:43 +0300

    Adding additional styling to events

--- MobileApp/app/home/events.jsx
+++ MobileApp/app/home/events.jsx
@@ -1,9 +1,12 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getGoogleCalendarEvents } from "../../functions/getGoogleCalendarEvents";
 import SingleEventBoxView from "../../components/Events/SingleEventBoxView";
+import { useUser } from "../../context/UserContext";
+
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 /*
   1. Opravi stilovo eventite
@@ -13,7 +16,12 @@ import SingleEventBoxView from "../../components/Events/SingleEventBoxView";
 
 export default function Events() {
   const { theme } = useTheme();
+  const { user } = useUser();
+
+  console.log(user);
+
   const [googleEvents, setGoogleEvents] = useState([]);
+  const [deviceCalendarEvents, setDeviceCalendarEvents] = useState([]);
 
   useEffect(() => {
     async function getGoogleToken() {
@@ -57,6 +65,24 @@ export default function Events() {
       marginLeft: 10,
       marginBottom: 10,
     },
+    eventsDiv: {
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+      gap: 20,
+    },
+    eventsDivTitle: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 15,
+      alignItems: "center",
+      alignSelf: "flex-start",
+      marginLeft: 10,
+    },
+    eventsDivTitleText: {
+      fontSize: 18,
+      color: theme.text,
+    },
   });
 
   return (
@@ -66,9 +92,22 @@ export default function Events() {
     >
       <Text style={styles.title}>Events</Text>
       <Text style={styles.subTitle}>{googleEvents.length} events</Text>
-      {googleEvents.map((event) => (
-        <SingleEventBoxView key={event.id} event={event} theme={theme} />
-      ))}
+      {user.google_sync && (
+        <View style={styles.eventsDiv}>
+          <View style={styles.eventsDivTitle}>
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require("../../assets/images/Google__G__logo.svg.png")}
+            />
+            <Text style={styles.eventsDivTitleText}>
+              Google calendar events
+            </Text>
+          </View>
+          {googleEvents.map((event) => (
+            <SingleEventBoxView key={event.id} event={event} theme={theme} />
+          ))}
+        </View>
+      )}
     </ScrollView>
   );
 }
