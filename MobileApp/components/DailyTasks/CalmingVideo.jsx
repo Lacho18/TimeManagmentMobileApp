7eb8d1d7b3a5ab5afb90007ca1c0071f9b4921d5
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-05-23 16:21:18 +0300

    Making component for calming video

--- MobileApp/components/DailyTasks/CalmingVideo.jsx
+++ MobileApp/components/DailyTasks/CalmingVideo.jsx
@@ -1,10 +1,19 @@
+import { Video } from "expo-av";
 import { StyleSheet, Text, View } from "react-native";
+//import YoutubePlayer from "react-native-youtube-iframe";
 
 export default function CalmingVideo({ theme }) {
   const styles = StyleSheet.create({
+    back: {
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      backgroundColor: theme.primary + "5D",
+      zIndex: 50,
+    },
     mainDiv: {
       position: "absolute",
-      width: "85%",
+      width: "100%",
       height: 300,
       top: "50%",
       left: "50%",
@@ -17,12 +26,32 @@ export default function CalmingVideo({ theme }) {
       justifyContent: "space-evenly",
       alignItems: "center",
       padding: 15,
+      zIndex: 60,
+      opacity: 1,
+    },
+    video: {
+      width: 320,
+      height: 200,
+      zIndex: 60,
     },
   });
 
   return (
-    <View style={styles.mainDiv}>
-      <Text>123</Text>
+    <View style={styles.back}>
+      <View style={styles.mainDiv}>
+        <Video
+          source={{
+            uri: "https://www.youtube.com/watch?v=eA9IvKdCA5w&ab_channel=Fiki",
+          }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay
+          useNativeControls
+          style={styles.video}
+        />
+      </View>
     </View>
   );
 }
